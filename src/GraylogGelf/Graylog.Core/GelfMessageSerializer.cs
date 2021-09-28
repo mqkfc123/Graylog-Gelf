a489b4dc8e8532644ae32e71975851c9f762dd33
liuyl <mqkfc123@163.com>
2021-09-28 14:19:06 +0800

    时区 时间戳修改

--- src/GraylogGelf/Graylog.Core/GelfMessageSerializer.cs
+++ src/GraylogGelf/Graylog.Core/GelfMessageSerializer.cs
@@ -12,14 +12,15 @@ namespace Graylog.Core
     {
         public byte[] Serialize(GelfMessage message)
         {
-            var duration = message.Timestamp.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var duration = message.Timestamp.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, 0); //DateTimeKind.Utc
             var result = new JsonObject
                 {
                     {"version", message.Version},
                     {"host", message.Host},
                     {"short_message", message.ShortMessage},
                     //{"full_message", message.FullMessage},
-                    {"timestamp", Math.Round(duration.TotalSeconds, 3, MidpointRounding.AwayFromZero)},
+                    //{"timestamp", Math.Round(duration.TotalSeconds, 3, MidpointRounding.AwayFromZero)},
+                    {"timestamp", duration},
                     {"level", (int)message.Level}
                 };
             foreach (var additionalField in message.AdditionalFields)
